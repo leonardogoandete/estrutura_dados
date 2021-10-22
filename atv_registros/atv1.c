9f8cb4fcfcfa1ba5554063733aca34ba17f930a4
Leonardo Goandete <leonardo.goandete@hotmail.com>
2021-10-21 22:08:53 -0300

    Erro compilar.

--- atv_registros/atv1.c
+++ atv_registros/atv1.c
@@ -16,7 +16,6 @@ int main(int argc, char const *argv[]) {
   int i; // percorrer o indice
   agenda p;
 // fim declaracao variaveis
-  p.aniver = 0;
   printf("Digite o nome:");
   gets(p.nome);
   printf("Digite o telefone:");
@@ -33,19 +32,3 @@ int main(int argc, char const *argv[]) {
   }
   return 0;
 }
-
-  // array pessoa
-/*
-  printf("%s - %d\n",p.nome,p.idade);
-  pessoa fam[3];
-  strcpy(fam[0].nome,"pai");
-  fam[0].idade=40;
-  strcpy(fam[1].nome,"mae");
-  fam[1].idade=39;
-  strcpy(fam[2].nome,"filho");
-  fam[2].idade=15;
-  for (i=0; i < 3; i++) {
-    printf("%s | %d\n",fam[i].nome,fam[i].idade);
-  }
-*/
-//}
