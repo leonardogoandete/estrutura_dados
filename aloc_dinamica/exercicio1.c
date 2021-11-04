2def01911c98b90440d4424b096a8ba644caa77d
Leonardo Goandete <leonardo.goandete@hotmail.com>
2021-11-03 23:03:33 -0300

    .

--- aloc_dinamica/exercicio1.c
+++ aloc_dinamica/exercicio1.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
   teste = (contato*)malloc(sizeof(contato));
   sizeof(contato);
   printf("%d",teste); // retorna o ponteiro da memoria
-  if(teste != 0){ // verifico se a memoria alocada é diferente de 0, se verdadeiro foi alocado memoria.
+  if(teste != 0 && teste != NULL){ // verifico se a memoria alocada é diferente de 0, se verdadeiro foi alocado memoria.
   gets((*teste).nome);
   strcpy((*teste).telefone, "1234567");
   printf("Nome = %s\nTelefone =  %s\n",(*teste).nome,(*teste).telefone);
